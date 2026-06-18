0d56a02b2d5c62519691f5a9b0f17fecac91858c
Ashen <ashen20060101@gmail.com>
2026-06-18 20:02:33 +0530

    student entity and repository creation

---
+++ backend/src/main/java/com/examflow/backend/entity/Student.java
@@ -0,0 +1,216 @@
+package com.examflow.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    private Integer studentSeq;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String guardianName;
+    private String guardianContactNumber;
+    private String contactNumber;
+    private String subjectStream;
+    private String district;
+    private String address;
+    private String password;
+    private String confirmPassword;
+    private String finalPassword;
+    private String email;
+    private String grade;
+    private String schoolName;
+    private String whatsappNumber;
+    private Boolean marketingConsent;
+    private Boolean termsAccepted;
+    private Integer status;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_seq")
+    public Integer getStudentSeq() {
+        return studentSeq;
+    }
+
+    public void setStudentSeq(Integer studentSeq) {
+        this.studentSeq = studentSeq;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "gender")
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Column(name = "guardian_name")
+    public String getGuardianName() {
+        return guardianName;
+    }
+
+    public void setGuardianName(String guardianName) {
+        this.guardianName = guardianName;
+    }
+
+    @Column(name = "guardian_contact_number")
+    public String getGuardianContactNumber() {
+        return guardianContactNumber;
+    }
+
+    public void setGuardianContactNumber(String guardianContactNumber) {
+        this.guardianContactNumber = guardianContactNumber;
+    }
+
+    @Column(name = "contact_number")
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Column(name = "subject_stream")
+    public String getSubjectStream() {
+        return subjectStream;
+    }
+
+    public void setSubjectStream(String subjectStream) {
+        this.subjectStream = subjectStream;
+    }
+
+    @Column(name = "district")
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "confirm_password")
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "grade")
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Column(name = "school_name")
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    @Column(name = "whatsapp_number")
+    public String getWhatsappNumber() {
+        return whatsappNumber;
+    }
+
+    public void setWhatsappNumber(String whatsappNumber) {
+        this.whatsappNumber = whatsappNumber;
+    }
+
+    @Column(name = "marketing_consent")
+    public Boolean getMarketingConsent() {
+        return marketingConsent;
+    }
+
+    public void setMarketingConsent(Boolean marketingConsent) {
+        this.marketingConsent = marketingConsent;
+    }
+
+    @Column(name = "terms_accepted")
+    public Boolean getTermsAccepted() {
+        return termsAccepted;
+    }
+
+    public void setTermsAccepted(Boolean termsAccepted) {
+        this.termsAccepted = termsAccepted;
+    }
+
+    @Column(name = "final_password")
+    public String getFinalPassword() {
+        return finalPassword;
+    }
+
+    public void setFinalPassword(String finalPassword) {
+        this.finalPassword = finalPassword;
+    }
+
+    @Column(name = "status")
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+}

a25c6943c6ced381e98ee3ff911323108871b0f9
Ashen <ashen20060101@gmail.com>
2026-06-18 20:03:14 +0530

    dto creation to get request from frontend and pass to frontend

---
+++ backend/src/main/java/com/examflow/backend/dto/UserSignUpRequest.java
@@ -0,0 +1,158 @@
+package com.examflow.backend.dto;
+
+public class UserSignUpRequest {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String guardianName;
+    private String guardianContactNumber;
+    private String contactNumber;
+    private String subjectStream;
+    private String district;
+    private String address;
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private String grade;
+    private String schoolName;
+    private String whatsappNumber;
+    private Boolean marketingConsent;
+    private Boolean termsAccepted;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGuardianName() {
+        return guardianName;
+    }
+
+    public void setGuardianName(String guardianName) {
+        this.guardianName = guardianName;
+    }
+
+    public String getGuardianContactNumber() {
+        return guardianContactNumber;
+    }
+
+    public void setGuardianContactNumber(String guardianContactNumber) {
+        this.guardianContactNumber = guardianContactNumber;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getSubjectStream() {
+        return subjectStream;
+    }
+
+    public void setSubjectStream(String subjectStream) {
+        this.subjectStream = subjectStream;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getWhatsappNumber() {
+        return whatsappNumber;
+    }
+
+    public void setWhatsappNumber(String whatsappNumber) {
+        this.whatsappNumber = whatsappNumber;
+    }
+
+    public Boolean getMarketingConsent() {
+        return marketingConsent;
+    }
+
+    public void setMarketingConsent(Boolean marketingConsent) {
+        this.marketingConsent = marketingConsent;
+    }
+
+    public Boolean getTermsAccepted() {
+        return termsAccepted;
+    }
+
+    public void setTermsAccepted(Boolean termsAccepted) {
+        this.termsAccepted = termsAccepted;
+    }
+}

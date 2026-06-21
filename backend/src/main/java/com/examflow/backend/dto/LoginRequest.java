94b51d99dd3473c41899e01e49f7d4ff2d442c24
Ashen <ashen20060101@gmail.com>
2026-06-21 19:18:23 +0530

    adding uername and email to get thoe from fronteend

--- backend/src/main/java/com/examflow/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/examflow/backend/dto/LoginRequest.java
@@ -4,14 +4,14 @@ public class LoginRequest {
 
     private String identifier;
 
+    private String username;
+
+    private String email;
+
     private String password;
 
     public String getIdentifier() {
         return identifier;
-    }
-
-    public void setUsername(String username) {
-        this.identifier = username;
     }
 
     public String getPassword() {
@@ -21,4 +21,25 @@ public class LoginRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
